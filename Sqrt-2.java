190d5ee7c19757bb99f76242a0427032fab6ea22
Digpal Singh Rathore <75355851+dsrathore1@users.noreply.github.com>
2026-02-20 09:34:18 +0530

    Create Sqrt-2.java

---
+++ Sqrt-2.java
@@ -0,0 +1,7 @@
+class Main {
+    public static void main(String[] args) {
+        int x = 56;
+        double squareRoot = Math.sqrt(x); // Store result in a double
+        System.out.println("The square root of " + x + " is: " + (int) squareRoot); 
+    }
+}
